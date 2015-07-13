d92fe53dca7363311c878a8fc80d0e806d85d0f1
fangkaijin <fangkaijin@gmail.com>
2015-07-13 10:17:50 +0800

    commit thirdparty  EDB

---
+++ erpservice-api/src/main/java/com/huobanplus/erpservice/thirdparty/utils/Contant.java
@@ -0,0 +1,34 @@
+package com.huobanplus.erpservice.thirdparty.utils;
+
+/**
+ * 常量定义
+ */
+public class Contant {
+    //EDB接口访问ip
+    private final static String HOST_IP = "http://vip802.6x86.com/edb2/rest/index.aspx";
+    //是否开启日志模式
+    private final static boolean IS_LOG = true;
+    //是否生产环境
+    private final static boolean IS_PRODUCTION = false;
+
+    //E店宝接口名称
+    /**
+     * 产品信息
+     */
+    //增加产品明细信息
+    private final static String EDB_PRODUCT_DETAIL_ADD = "edbProductDetailAdd";
+    //更新产品明细信息
+    private final static String EDB_PRODUCT_DETAIL_INFO_UPDATE = "edbProductDetailInfoUpdate";
+    //获取产品分类信息
+    private final static String EDB_PRODUCT_DETAIL_CLASS_GET = "edbProductClassGet";
+    //增加产品品牌
+    private final static String EDB_PRODUCT_BRAND_ADD = "edbProductBrandAdd";
+    //获取产品基本产品信息
+    private final static String EDB_PRODUCT_BASE_INFO_GET = "edbProductBaseInfoGet";
+    //获取产品库存信息
+    private final static String EDB_PRODUCT_GET = "edbProductGet";
+    /**
+     * 出入库信息
+     */
+    //
+}
