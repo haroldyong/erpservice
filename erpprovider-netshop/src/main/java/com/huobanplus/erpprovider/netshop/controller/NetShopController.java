1628dbb3adb460c4f172e469d1bff3910b9ed684
fangkaijin <fangkaijin@gmail.com>
2015-07-24 13:52:56 +0800

    commit netshop fixed

---
+++ erpprovider-netshop/src/main/java/com/huobanplus/erpprovider/netshop/controller/NetShopController.java
@@ -0,0 +1,10 @@
+package com.huobanplus.erpprovider.netshop.controller;
+
+/**
+ * Created by Administrator on 2015/7/24.
+ */
+public class NetShopController {
+
+
+
+}
---
+++ erpprovider-netshop/src/main/java/com/huobanplus/erpprovider/netshop/util/Constant.java
@@ -0,0 +1,23 @@
+package com.huobanplus.erpprovider.netshop.util;
+
+/**
+ * 常量工具类
+ */
+public class Constant {
+
+    //---订单
+    //订单查询
+    //订单详细
+
+
+    //---发货通知
+
+
+    //---商品
+    //商品查询
+
+
+    //---库存同步接口
+
+
+    }
