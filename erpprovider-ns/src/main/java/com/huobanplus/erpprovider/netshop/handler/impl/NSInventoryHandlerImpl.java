1c84216e8f3ebdd77c542d8433e4d742f7ac8b02
fangkaijin <fangkaijin@gmail.com>
2015-08-05 14:19:34 +0800

    netshop service

--- erpprovider-ns/src/main/java/com/huobanplus/erpprovider/netshop/handler/impl/NSInventoryHandlerImpl.java
+++ erpprovider-ns/src/main/java/com/huobanplus/erpprovider/netshop/handler/impl/NSInventoryHandlerImpl.java
@@ -31,42 +31,45 @@ public class NSInventoryHandlerImpl implements NSInventoryHandler {
     private MallInventoryRepository mallInventoryRepository;
     @Override
     public Monitor<EventResult> synsInventory(HttpServletRequest request) throws IOException {
+
+        String secret = (String) request.getAttribute("secret");
+        String sign = (String) request.getAttribute("sign");
         Map<String, String> signMap = new TreeMap<>();
         signMap.put("uCode", request.getParameter(Constant.SIGN_U_CODE));
         signMap.put("mType", request.getParameter(Constant.SIGN_M_TYPE));
         signMap.put("TimeStamp", request.getParameter(Constant.SIGN_TIME_STAMP));
-        signMap.put("ItemID", request.getParameter("ItemID"));
-        signMap.put("SkuID", request.getParameter("SkuID"));
-        signMap.put("Quantity", request.getParameter("Quantity"));
-        String sign = SignBuilder.buildSign(signMap, Constant.SECRET, Constant.SECRET);
-        if (!sign.toUpperCase().equals(request.getParameter("Sign"))) {
+
+        Map<String, String> inventoryMap = new TreeMap<>();
+        inventoryMap.put("ItemID", request.getParameter("ItemID"));
+        inventoryMap.put("SkuID", request.getParameter("SkuID"));
+        inventoryMap.put("Quantity", request.getParameter("Quantity"));
+        inventoryMap.put("inventoryNo", request.getParameter("inventoryNo"));
+
+        String signStr = SignBuilder.buildSign(signMap, secret, secret);
+        if (null != signStr && signStr.equals(sign)) {
             return new BaseMonitor<>(new EventResult(0,
                     "<?xml version='1.0' encoding='utf-8'?><Rsp><Result>0</result><GoodsType></GoodsType><Cause>签名不正确</Cause></Rsp>"));
         }
+        else
+        {
+            // ObjectMapper objectMapper = new ObjectMapper();
+            //String resultJson = objectMapper.writeValueAsString(responseMap);
+            //记录需要更新的库存信息
+            MallInventoryBean mallInventoryBean = new MallInventoryBean();
+            mallInventoryBean.setProductItemNo(inventoryMap.get("itemId"));
+            mallInventoryBean.setSkuId(inventoryMap.get("skuID"));
+            mallInventoryBean.setStorageNum(Integer.parseInt(inventoryMap.get("quantity")));
+            mallInventoryBean.setInventoryNo(inventoryMap.get("inventoryNo"));
 
-        Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("itemId", signMap.get("ItemID"));
-        responseMap.put("skuID", signMap.get("SkuID"));
-        responseMap.put("quantity", signMap.get("Quantity"));
+            //更新或保存当前的库存信息
+            MallInventoryBean result = mallInventoryRepository.save(mallInventoryBean);
 
-       // ObjectMapper objectMapper = new ObjectMapper();
-        //String resultJson = objectMapper.writeValueAsString(responseMap);
-        //记录需要更新的库存信息
-        MallInventoryBean mallInventoryBean = new MallInventoryBean();
-        mallInventoryBean.setProductItemNo(responseMap.get("itemId"));
-        mallInventoryBean.setSkuId(responseMap.get("skuID"));
-        mallInventoryBean.setStorageNum(Integer.parseInt(responseMap.get("quantity")));
-
-
-        //String result = HttpUtil.getInstance().doPost(null, null);
-        //保存当前更新的信息
-        MallInventoryBean result = mallInventoryRepository.save(mallInventoryBean);
-
-
-        if (result == null) {
-            return new BaseMonitor<>(new EventResult(0,
-                    "<?xml version='1.0' encoding='utf-8'?><Rsp><Result>0</result><GoodsType></GoodsType><Cause>客户端请求失败</Cause></Rsp>"));
+            if (result == null) {
+                return new BaseMonitor<>(new EventResult(0,
+                        "<?xml version='1.0' encoding='utf-8'?><Rsp><Result>0</result><GoodsType></GoodsType><Cause>客户端请求失败</Cause></Rsp>"));
+            }
+            return new BaseMonitor<>(new EventResult(1, "<?xml version='1.0' encoding='utf-8'?><Rsp><Result>1</result><GoodsType></GoodsType><Cause>需要更新的数据已经数据已经保存</Cause></Rsp>"));
         }
-        return new BaseMonitor<>(new EventResult(1, "<?xml version='1.0' encoding='utf-8'?><Rsp><Result>1</result><GoodsType></GoodsType><Cause>需要更新的数据已经数据已经保存</Cause></Rsp>"));
+
     }
 }
