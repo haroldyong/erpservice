a2744b86ad4285a816b0761a7d188153a139de05
fangkaijin <fangkaijin@gmail.com>
2015-08-18 09:06:03 +0800

    commit fixed netshop

--- erpservice-api/src/main/java/com/huobanplus/erpservice/polling/ApiPolling.java
+++ erpservice-api/src/main/java/com/huobanplus/erpservice/polling/ApiPolling.java
@@ -16,8 +16,9 @@ public class ApiPolling {
      * Spring Task 轮询测试
      * 每天每隔2小时轮询一次 cron 可配置
      * @Scheduled(cron = "0 0 0/2 * * ?")
+     * //cron = "0 0/1 * * * ?",
      */
-    @Scheduled(fixedRate = 60000,initialDelay = 60000)//cron = "0 0/1 * * * ?",
+    @Scheduled(fixedRate = 60000,initialDelay = 60000)
     private void polling()
     {
         //
