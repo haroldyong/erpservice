a120eac8f8d8e5b663e2942a2929ea13a61fc6b7
allan <lqc_huli@163.com>
2016-06-07 17:06:18 +0800

    1.xxx

--- erpprovider-kjyg/src/test/java/com/huobanplus/erpprovider/kjyg/handler/KjygOrderHandlerTest.java
+++ erpprovider-kjyg/src/test/java/com/huobanplus/erpprovider/kjyg/handler/KjygOrderHandlerTest.java
@@ -1,3 +1,12 @@
+/*
+ * 版权所有:杭州火图科技有限公司
+ * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼
+ *
+ * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ * 2013-2016. All rights reserved.
+ */
+
 package com.huobanplus.erpprovider.kjyg.handler;
 
 import com.alibaba.fastjson.JSON;
@@ -70,8 +79,6 @@ public class KjygOrderHandlerTest extends KjygTestBase {
         mockOrder.setUserLoginName("18705153967");
         mockOrder.setBuyerPid("362322199411050053");
         mockOrder.setBuyerName("wuxiongliu");
-        mockOrder.setPayWay(1);
-        mockOrder.setFhArea("日本");
         mockOrder.setShipZip("310000");
         mockOrder.setShipArea("滨江区");
         mockOrder.setRemark("test");
