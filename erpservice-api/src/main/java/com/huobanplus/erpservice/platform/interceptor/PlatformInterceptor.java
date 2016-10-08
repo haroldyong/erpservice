e913c24e3a3f21c49214146340f1912d370fd700
allan <lqc_huli@163.com>
2016-10-08 22:49:17 +0800

    修改验证规则,现在超级管理员也能查看了.

--- erpservice-api/src/main/java/com/huobanplus/erpservice/platform/interceptor/PlatformInterceptor.java
+++ erpservice-api/src/main/java/com/huobanplus/erpservice/platform/interceptor/PlatformInterceptor.java
@@ -34,21 +34,24 @@ public class PlatformInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String erpUserType = request.getParameter("erpUserType");
+
         Integer customerId;
         if (erpUserType.equals("0")) {
-            customerId = CookieHelper.getCookieValInteger(request, "UserID");
+            String requestCustomerId = request.getParameter("customerid");
+            if (StringUtils.isEmpty(requestCustomerId)) {
+                customerId = CookieHelper.getCookieValInteger(request, "UserID");
+                if (customerId == 0) {
+                    response.sendRedirect(environment.getProperty("huobanplus_login", "http://localhost:8080"));
+                    return false;
+                }
+            } else {
+                customerId = Integer.valueOf(requestCustomerId);
+            }
+
             if (environment.acceptsProfiles("development")) {
                 customerId = 296;
             }
 
-            if (customerId == null || customerId == 0) {
-                String requestCustomerId = request.getParameter("customerid");
-                if (StringUtils.isEmpty(requestCustomerId)) {
-                    response.sendRedirect(environment.getProperty("huobanplus_login", "http://localhost:8080"));
-                    return false;
-                }
-                customerId = Integer.valueOf(requestCustomerId);
-            }
 
 //            request.setAttribute("customerId", );
         } else {
