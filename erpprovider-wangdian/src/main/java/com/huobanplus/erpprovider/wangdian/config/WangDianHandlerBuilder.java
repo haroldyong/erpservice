fc42448dc2ca4f89a007f5761f902502fe23e453
wuxiongliu <xiong328160186@qq.com>
2017-01-13 09:29:36 +0800

    fix wangdian bug

--- erpprovider-wangdian/src/main/java/com/huobanplus/erpprovider/wangdian/config/WangDianHandlerBuilder.java
+++ erpprovider-wangdian/src/main/java/com/huobanplus/erpprovider/wangdian/config/WangDianHandlerBuilder.java
@@ -56,87 +56,89 @@ public class WangDianHandlerBuilder implements ERPHandlerBuilder {
 
     @Override
     public ERPHandler buildHandler(ERPInfo info) {
-        return new ERPHandler() {
-            @Override
-            public EventResult handleEvent(ERPBaseEvent erpBaseEvent) {
-                if (erpBaseEvent instanceof PushNewOrderEvent) {
-                    PushNewOrderEvent pushNewOrderEvent = (PushNewOrderEvent) erpBaseEvent;
-                    return wangDianOrderHandler.pushOrder(pushNewOrderEvent);
-                }
-                return null;
-            }
-
-            @Override
-            public EventResult handleRequest(HttpServletRequest request, ERPTypeEnum.ProviderType providerType, ERPTypeEnum.UserType erpUserType) {
-
-                String method = request.getParameter("Method");
-                String sellerID = request.getParameter("SellerID");
-                String requestSign = request.getParameter("Sign");
-                String content = request.getParameter("Content");
-
-                log.info("wangdian delivery content:" + content);
-                try {
-
-                    List<ERPSysDataInfo> sysDataInfos = sysDataInfoService.findByErpTypeAndErpUserTypeAndParamNameAndParamVal(providerType, erpUserType, "sellerId", sellerID);
-                    if (sysDataInfos.size() == 0) {
-                        JSONObject respJson = new JSONObject();
-                        respJson.put("ResultCode", 1);
-                        respJson.put("ResultMsg", "未找到该商户的相关配置");
-
-                        JSONObject response = new JSONObject();
-                        response.put("ResultList", respJson);
-                        return EventResult.resultWith(EventResultEnum.ERROR, response);
+        if (info.getErpType() == ERPTypeEnum.ProviderType.WANGDIAN) {
+            return new ERPHandler() {
+                @Override
+                public EventResult handleEvent(ERPBaseEvent erpBaseEvent) {
+                    if (erpBaseEvent instanceof PushNewOrderEvent) {
+                        PushNewOrderEvent pushNewOrderEvent = (PushNewOrderEvent) erpBaseEvent;
+                        return wangDianOrderHandler.pushOrder(pushNewOrderEvent);
                     }
-                    ERPDetailConfigEntity erpDetailConfig = detailConfigService.findBySysData(sysDataInfos, providerType, erpUserType);
-                    WangDianSysData wangDianSysData = JSON.parseObject(erpDetailConfig.getErpSysData(), WangDianSysData.class);
+                    return null;
+                }
 
-                    ERPUserInfo erpUserInfo = new ERPUserInfo(erpUserType, erpDetailConfig.getCustomerId());
-                    ERPInfo erpInfo = new ERPInfo(erpDetailConfig.getErpType(), erpDetailConfig.getErpSysData());
+                @Override
+                public EventResult handleRequest(HttpServletRequest request, ERPTypeEnum.ProviderType providerType, ERPTypeEnum.UserType erpUserType) {
 
-                    // 签名验证
+                    String method = request.getParameter("Method");
+                    String sellerID = request.getParameter("SellerID");
+                    String requestSign = request.getParameter("Sign");
+                    String content = request.getParameter("Content");
 
-                    String sign = WangDianSignUtil.buildSign(content, wangDianSysData.getAppKey());
-                    if (sign.equals(requestSign)) {
+                    log.info("wangdian delivery content:" + content);
+                    try {
 
-                        switch (method) {
-                            case Constant.LOGISTICS_RETURN:
-                                JSONObject jsonObject = JSON.parseObject(content);
-                                JSONObject tradeList = jsonObject.getJSONObject("TradeList");
-                                JSONArray jsonArray = tradeList.getJSONArray("Trade");
+                        List<ERPSysDataInfo> sysDataInfos = sysDataInfoService.findByErpTypeAndErpUserTypeAndParamNameAndParamVal(providerType, erpUserType, "sellerId", sellerID);
+                        if (sysDataInfos.size() == 0) {
+                            JSONObject respJson = new JSONObject();
+                            respJson.put("ResultCode", 1);
+                            respJson.put("ResultMsg", "未找到该商户的相关配置");
 
-                                List<WangDianLogistic> wangDianLogistics = new ArrayList<>();
-
-                                jsonArray.forEach(obj -> {
-                                    WangDianLogistic wangDianLogistic = JSON.parseObject(obj.toString(), WangDianLogistic.class);
-                                    wangDianLogistics.add(wangDianLogistic);
-                                });
-                                return wangDianOrderHandler.deliverOrder(wangDianLogistics, erpUserInfo, erpInfo);
+                            JSONObject response = new JSONObject();
+                            response.put("ResultList", respJson);
+                            return EventResult.resultWith(EventResultEnum.ERROR, response);
                         }
+                        ERPDetailConfigEntity erpDetailConfig = detailConfigService.findBySysData(sysDataInfos, providerType, erpUserType);
+                        WangDianSysData wangDianSysData = JSON.parseObject(erpDetailConfig.getErpSysData(), WangDianSysData.class);
 
-                    } else {
+                        ERPUserInfo erpUserInfo = new ERPUserInfo(erpUserType, erpDetailConfig.getCustomerId());
+                        ERPInfo erpInfo = new ERPInfo(erpDetailConfig.getErpType(), erpDetailConfig.getErpSysData());
+
+                        // 签名验证
+
+                        String sign = WangDianSignUtil.buildSign(content, wangDianSysData.getAppKey());
+                        if (sign.equals(requestSign)) {
+
+                            switch (method) {
+                                case Constant.LOGISTICS_RETURN:
+                                    JSONObject jsonObject = JSON.parseObject(content);
+                                    JSONObject tradeList = jsonObject.getJSONObject("TradeList");
+                                    JSONArray jsonArray = tradeList.getJSONArray("Trade");
+
+                                    List<WangDianLogistic> wangDianLogistics = new ArrayList<>();
+
+                                    jsonArray.forEach(obj -> {
+                                        WangDianLogistic wangDianLogistic = JSON.parseObject(obj.toString(), WangDianLogistic.class);
+                                        wangDianLogistics.add(wangDianLogistic);
+                                    });
+                                    return wangDianOrderHandler.deliverOrder(wangDianLogistics, erpUserInfo, erpInfo);
+                            }
+
+                        } else {
+                            JSONObject respJson = new JSONObject();
+                            respJson.put("ResultCode", 1);
+                            respJson.put("ResultMsg", "签名错误");
+
+                            JSONObject response = new JSONObject();
+                            response.put("ResultList", respJson);
+
+                            return EventResult.resultWith(EventResultEnum.ERROR, response);
+                        }
+                    } catch (Exception e) {
                         JSONObject respJson = new JSONObject();
                         respJson.put("ResultCode", 1);
-                        respJson.put("ResultMsg", "签名错误");
+                        respJson.put("ResultMsg", e.getMessage());
 
                         JSONObject response = new JSONObject();
                         response.put("ResultList", respJson);
 
                         return EventResult.resultWith(EventResultEnum.ERROR, response);
                     }
-                } catch (Exception e) {
-                    JSONObject respJson = new JSONObject();
-                    respJson.put("ResultCode", 1);
-                    respJson.put("ResultMsg", e.getMessage());
-
-                    JSONObject response = new JSONObject();
-                    response.put("ResultList", respJson);
-
-                    return EventResult.resultWith(EventResultEnum.ERROR, response);
+                    return null;
                 }
-
-                return null;
-            }
-        };
+            };
+        }
+        return null;
     }
 
 
