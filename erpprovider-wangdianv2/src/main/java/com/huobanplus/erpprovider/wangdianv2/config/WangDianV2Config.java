f76843466c0f0c6fd7aab4cd0986980d7aeb694a
wxl <xiong328160186@qq.com>
2017-03-08 16:42:21 +0800

    奇怪的文件名改变为问题

---
+++ erpprovider-wangdianv2/src/main/java/com/huobanplus/erpprovider/wangdianv2/config/WangDianV2Config.java
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * 版权所有:杭州火图科技有限公司
+ *  * 地址:浙江省杭州市滨江区西兴街道阡陌路智慧E谷B幢4楼在地图中查看
+ *  *
+ *  * (c) Copyright Hangzhou Hot Technology Co., Ltd.
+ *  * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
+ *  * 2013-2016. All rights reserved.
+ *
+ */
+
+package com.huobanplus.erpprovider.wangdianv2.config;
+
+import com.huobanplus.erpservice.eventhandler.ERPRegister;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class WangDianV2Config {
+
+    @Autowired
+    private ERPRegister erpRegister;
+
+    @Autowired
+    private WangDianV2HandlerBuilder wangDianV2HandlerBuilder;
+
+    @PostConstruct
+    public void init() {
+        erpRegister.addBuilders(wangDianV2HandlerBuilder);
+    }
+
+}
