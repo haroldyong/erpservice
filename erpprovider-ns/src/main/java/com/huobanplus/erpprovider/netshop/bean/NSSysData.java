99323c7729640eb70f558a09a64a88183b62cb3f
allan <lqc_huli@163.com>
2017-07-31 14:59:19 +0800

    nssysdata 修改为String

--- erpprovider-ns/src/main/java/com/huobanplus/erpprovider/netshop/bean/NSSysData.java
+++ erpprovider-ns/src/main/java/com/huobanplus/erpprovider/netshop/bean/NSSysData.java
@@ -4,7 +4,7 @@
  *
  * (c) Copyright Hangzhou Hot Technology Co., Ltd.
  * Floor 4,Block B,Wisdom E Valley,Qianmo Road,Binjiang District
- * 2013-2015. All rights reserved.
+ * 2013-2017. All rights reserved.
  */
 
 package com.huobanplus.erpprovider.netshop.bean;
@@ -19,7 +19,7 @@ public class NSSysData {
     /**
      * <p>接入码，用于验证请求的有效性。主要用于区分店铺</p>
      */
-    private int uCode;
+    private String uCode;
     /**
      * <p>Secret是密钥，会在esAPI里面填写，商家自己这边需要设置，不会进行参数传值</p>
      */
