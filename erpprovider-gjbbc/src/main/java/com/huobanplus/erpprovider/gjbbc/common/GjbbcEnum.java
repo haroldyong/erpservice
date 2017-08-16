58cde24810f04a5c3febcc6ec8a685647449c6a1
huxuanhao2017 <hu_xuanhao@163.com>
2017-08-16 15:49:19 +0800

    高捷BBC接口对接

---
+++ erpprovider-gjbbc/src/main/java/com/huobanplus/erpprovider/gjbbc/common/GjbbcEnum.java
@@ -0,0 +1,22 @@
+package com.huobanplus.erpprovider.gjbbc.common;
+
+/**
+ * Created by hxh on 2017-08-15.
+ */
+public interface GjbbcEnum {
+    enum CustomerEnum {
+        HANGZHOU,
+        ZONGSHU,
+        ZHENGZHOU,
+        GUANGZHOU,
+        CHONGQING,
+        NINGBO,
+        SHENZHEN,
+        HENAN,
+        SHANGHAI,
+        XIAN,
+        FUJIAN,
+        TIANJIN,
+        NANSHAGJ
+    }
+}
